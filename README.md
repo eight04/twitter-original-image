b0307bcc2281bc310ea51fbb0525f085192dd6d4
eight <eight04@gmail.com>
2016-12-03 01:59:58 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Twitter Original Image
+======================
+Add ":orig" to twitter images.
+
+This script should be used with [this style](https://userstyles.org/styles/135922/twitter-modal-full-width).
+
+Github
+------
+<https://github.com/eight04/twitter-original-image>
---
+++ twitter-original-image.user.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name        Twitter Original Image
+// @description Add ":orig" to twitter images.
+// @namespace   eight04.blogspot.com
+// @include     https://twitter.com/*
+// @version     0.0.0
+// @license		MIT
+// @supportURL	https://github.com/eight04/twitter-original-image/issues
+// @compatible	Firefox
+// @grant       none
+// ==/UserScript==
+
+var id;
+function replace() {
+	var el = document.querySelector("#permalink-overlay [data-item-id]");
+	if (!el) {
+		id = null;
+		return;
+	}
+	var newId = el.dataset.itemId;
+	if (newId == id) {
+		return;
+	}
+	id = newId;
+	
+	var imgs = document.querySelectorAll("#permalink-overlay .AdaptiveMedia img"),
+		img;
+	
+	for (img of imgs) {
+		img.src += ":orig";
+	}
+}
+
+replace();
+
+new MutationObserver(replace).observe(document.querySelector("#permalink-overlay"), {
+	childList: true,
+	subtree:true
+});
